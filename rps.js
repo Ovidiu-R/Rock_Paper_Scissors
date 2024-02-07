96ce3a96d17226e4046f775632dc181973f45072
Ovidiu-R <ovidiu.m.radulescu@gmail.com>
2024-02-07 19:24:43 +0000

    Finish project

--- rps.js
+++ rps.js
@@ -1,10 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-
 const choices = ["rock", "paper", "scissors"];
 let buttons = document.querySelectorAll("button");
 let roundResult = document.getElementById('roundResult');
 let score = document.getElementById('score');
 let winner = document.getElementById('winner');
+let scores = {player: 0, computer: 0};
+let restart = document.getElementById('restart');
 buttons.forEach(button => {
     button.addEventListener('mouseover', () => {
         button.style.backgroundColor = 'gold';
@@ -14,27 +15,25 @@ buttons.forEach(button => {
         button.style.backgroundColor = '';
     });
     button.addEventListener('click', (e) => {
-        getHumanChoice(e.target.id);
-        playGame();
+        playGame(e.target.id);
     });
 });
 
-function playGame() {
-    let scores = {player: 0, computer: 0};
-        // while (scores.player < 5 && scores.computer < 5){
-            playRound (getComputerChoice(), getHumanChoice(), scores);
-        // }
+function playGame(button_id) {
+        playRound (getComputerChoice(), getHumanChoice(button_id));
         if (scores.player >= 5){
             winner.textContent = "Player wins!";
+            reset(scores);
         } else if (scores.computer >= 5){
             winner.textContent = "Computer wins!";
+            reset(scores);
         }
 }
 
-function playRound (computerSelection, humanSelection, scores) {
+function playRound (computerSelection, humanSelection) {
     if (computerSelection === humanSelection) {
         roundResult.textContent = "Try again!"; 
-        return playRound (getComputerChoice(), getHumanChoice(), scores);
+        return playRound (getComputerChoice(), getHumanChoice());
     }
         else if (computerSelection === "rock" && humanSelection === "paper"){
             roundResult.textContent = "Paper beats rock!";
@@ -75,10 +74,22 @@ function getComputerChoice () {
     return (choices[randomIndex]);
 }
 
-function getHumanChoice(choice) {
-        return choice;
+function getHumanChoice(button_id) {
+        return button_id;
                
 }
 
 
 });
+
+function reset(scores){
+    restart.removeAttribute('hidden');
+    restart.addEventListener ('click', () => {
+        restart.setAttribute("hidden", "hidden");
+        scores.player = 0; 
+        scores.computer = 0;
+        roundResult.textContent = "Rock, paper, scissors, shoot!";
+        score.textContent = `${scores.player} - ${scores.computer}`;
+        winner.textContent = "";
+    });
+}    
